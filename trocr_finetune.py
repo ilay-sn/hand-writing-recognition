cdd034385503092ffefef44cddc8fbf14081dc3d
ilayda <ilayda.snck.02@gmail.com>
2025-05-06 14:43:08 +0300

    trocr inference code

---
+++ trocr_finetune.py
@@ -0,0 +1,17 @@
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+from PIL import Image
+import torch
+import requests
+
+# load image from the IAM database
+url = '/home/ilayda/Pictures/Screenshots/Screenshot_from_2025-05-06_10-44-07.png'
+image = Image.open(url).convert("RGB")
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+processor = TrOCRProcessor.from_pretrained('microsoft/trocr-small-handwritten', use_fast=False)
+model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-small-handwritten')
+pixel_values = processor(images=image, return_tensors="pt").pixel_values
+
+generated_ids = model.generate(pixel_values)
+generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+print(generated_text)
